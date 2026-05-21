3d9acc44eda45f26bb413078f13b4101b9060ee6
Siddharth-singh18 <siddharthsinghdeoria@gmail.com>
2026-05-21 11:32:09 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        map<int,int>mp;
+        for(int i=0;i<n;i++) mp[nums[i]]++;
+        for(auto &it:mp){
+            if(it.second>1) return true;
+        }
+
+        return false;
+    }
+};
